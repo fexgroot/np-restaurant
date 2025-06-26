275899c5897daf804ccc017898c72dbf8182d929
Felix Nagy <fexgroot@gmail.com>
2025-06-26 13:38:33 +0200

    title added in html header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type React from "react"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
+import type React from "react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="nl">
+      <head>
+        <title>NP-Restaurant</title>
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
